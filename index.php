6493f5c2a6a98519214c66bf7980ec2d27edeecb
RuiterM <302078305@student.rocmondriaan.nl>
2022-11-24 15:24:53 +0100

    header

--- index.php
+++ index.php
@@ -1,1 +1,47 @@
-<?php
+<?php ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Auto4U</title>
+</head>
+<body>
+<!--header-->
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <div class="container-fluid">
+        <a href="index.php">
+        <img class="wheel" src="img/wheel.png">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="Catogorien.php">Catogoriën</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-light" href="sushifotos.php">Sushis</a>
+                </li>
+                <li class="nav-item me-auto">
+                    <a class="nav-link text-light" href="sushifotos.php">Login</a>
+                </li>
+                <li class="nav-item me-auto">
+                    <a class="nav-link text-light" href="sushifotos.php">Registeer</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div><img src="img/header.jpg" class="w-100" style="height: 200px; object-fit: cover;"></div>
+<!--header-->
+</body>
+</html>
